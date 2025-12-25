11443d189c9f2fa65837dcdd20af24b919d4b90f
sourav-kr14 <souravkr93@gmail.com>
2025-12-25 19:26:07 +0530

    fix :minor fix for db

--- backend/app/Models/Article.php
+++ backend/app/Models/Article.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 class Article extends Model
 {
@@ -18,4 +19,16 @@ class Article extends Model
         'version',
         'references',
     ];
+
+    protected $casts = [
+        'references' => 'array',
+        'version' => 'integer',
+    ];
+
+   
+    protected static function boot()
+    {
+        parent::boot();
+        static::creating(fn ($article) => $article->slug ??= Str::slug($article->title));
+    }
 }
